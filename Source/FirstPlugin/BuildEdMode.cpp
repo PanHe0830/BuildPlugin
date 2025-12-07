57e1723bf4198d9be02c16f65df8dfd24b84d3ee
panhe125801@gmail.com <panhe125801@gmail.com>
2025-12-07 19:38:57 +0800

    add source

---
+++ Source/FirstPlugin/BuildEdMode.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BuildEdMode.h"
+
+BuildEdMode::BuildEdMode()
+{
+}
+
+BuildEdMode::~BuildEdMode()
+{
+}
