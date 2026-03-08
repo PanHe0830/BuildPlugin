5efce662c5fa4fd002cd2fd4461fb77ebea4693b
PH <panhe125801@gmail.com>
2026-03-08 21:18:08 +0800

    构建插件添加压缩文件

---
+++ Build/PlacementBuildEditor/Source/PlacementBuildEditor/Private/ToolKit/BuildToolkitWidget.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Types/BuildModeTypes.h"
+#include "Widgets/SCompoundWidget.h"
+
+class SBuildToolkitWidget : public SCompoundWidget
+{
+public:
+    SLATE_BEGIN_ARGS(SBuildToolkitWidget) {}
+        SLATE_EVENT(FOnBuildModeChanged, OnBuildModeChange)
+        SLATE_EVENT(FOnBuildAssetChanged, OnAssetChanged)
+    SLATE_END_ARGS()
+
+    void Construct(const FArguments& InArgs);
+
+private:
+    FOnBuildModeChanged OnBuildModeChange;
+    FOnBuildAssetChanged OnAssetChanged;
+};
